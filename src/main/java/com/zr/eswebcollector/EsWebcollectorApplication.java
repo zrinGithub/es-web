1fab30773ac7b3a3a4afc585584bc64b3c27f4f8
zhangrui <zhangrui1@cmiot.chinamobile.com>
2019-11-13 10:54:49 +0800

    [init] base structure

---
+++ src/main/java/com/zr/eswebcollector/EsWebcollectorApplication.java
@@ -0,0 +1,13 @@
+package com.zr.eswebcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsWebcollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsWebcollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zr/eswebcollector/EsWebcollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zr.eswebcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsWebcollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
